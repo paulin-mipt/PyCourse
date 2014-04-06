77f1a3d87388dc9bd1d12d154ce20462f9c46117
paulin-mipt <paulin.mipt@gmail.com>
2014-04-06 23:49:11 +0400

    -output bug in pinger

--- Task4/task4_pinger.py
+++ Task4/task4_pinger.py
@@ -6,10 +6,10 @@ def ping_host(host, num):
     PIPE = subprocess.PIPE
     p = subprocess.Popen(" ".join(["ping", host]), shell=True, stdin=PIPE, stdout=PIPE,
         stderr=subprocess.STDOUT, close_fds=True)
-    answer = " "
+    answer = p.stdout.readline()
     while answer:
+        print('thread', num, 'received answer:  ', str(answer)[2:-3])
         answer = p.stdout.readline()
-        print('thread', num, 'received answer', answer[:-1])
 
 parser = argparse.ArgumentParser(description='Pinger v.0.0.9')
 parser.add_argument('hosts', metavar = 'host', type = str, nargs = '+', 
